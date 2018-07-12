6377736964e4188c0151eb68c6363353b22112b7
Lars <lars.bauhofer@gmail.com>
2018-07-12 23:07:38 +0200

    added arm support for the installation

--- install/install.sh
+++ install/install.sh
@@ -2,7 +2,6 @@
 
 ROOT_UID=0
 JAVA_VERSION=1.8
-RXTX_FOLDER=./util/rxtx
 RXTX_SHARED_OBJECT_LOCATION=/usr/lib/jni/
 INSTALL_PATH=/opt/RFID_detection/
 LOCAL_INSTALL_FILE=rfid-detection-0.0.1-jar-with-dependencies.jar
@@ -11,18 +10,13 @@ LOCAL_INSTALL_FILE=rfid-detection-0.0.1-jar-with-dependencies.jar
 REPO_URL='https://github.com/keinproblem/rfiddetection'
 ARTIFACT_NAME='rfid-detection-0.0.1-jar-with-dependencies.jar'
 
+
 #check for correct inputs
 if  [ "$#" != "0" ] && [ "$1" != "-o" ]; then
     echo "Unsupported input parameter!"
     echo "Allowed parameter:"
     echo "-o to use the JAR inside bin, othwise it will download the latest from git"
     echo "e.g. ./install.sh -o"
-    exit
-fi
-
-#check for correct inputs
-if  [ "$#" != "0" ] && [ "$1" != "-o" ]; then
-    echo $1
     exit
 fi
 
@@ -44,7 +38,7 @@ esac
 
 # check if java is installed
 if type -p java; then
-    _java=java
+    _java=javadwWWWWwww
 elif [[ -n "${JAVA_HOME}" ]] && [[ -x "${JAVA_HOME}/bin/java" ]];  then     
     _java="${JAVA_HOME}/bin/java"
 else
@@ -65,32 +59,14 @@ fi
 # check if RXTXcomm Library is installed
 case "${machine}" in
     Linux)
-	txrxParallelSo=$(ls ${RXTX_SHARED_OBJECT_LOCATION}/librxtxParallel*.so 2> /dev/null | wc -l)
-        txrxSerialSo=$(ls ${RXTX_SHARED_OBJECT_LOCATION}/librxtxSerial*.so 2> /dev/null | wc -l)
-
-	if [[ $(uname -m) == "x86_64" ]]; then
-	    rxtxLibraryFolder=${RXTX_FOLDER}/x86_64
-    	else
-            rxtxLibraryFolder=${RXTX_FOLDER}/x86
-	    if [ "${txrxParallelSo}" == 0 ]; then
-   	        cp ${rxtxLibraryFolder}/librxtxParallel.so ${RXTX_SHARED_OBJECT_LOCATION}
-	    fi
-    	fi
-
-	if [ "${txrxSerialSo}" != 0 ]; then
-	    cp ${rxtxLibraryFolder}/librxtxSerial.so ${RXTX_SHARED_OBJECT_LOCATION}
-	fi
-	;;
-
-    Mac)
-	txrxSerialSo=$(ls ${RXTX_SHARED_OBJECT_LOCATION}/librxtxSerial*.jnilib 2> /dev/null | wc -l)
-	rxtxLibraryFolder=${RXTX_FOLDER}/mac
-
-	if [ "${txrxSerialSo}" != 0 ]; then
-	    cp ${rxtxLibraryFolder}/librxtxSerial.jnilib ${RXTX_SHARED_OBJECT_LOCATION}
-	fi
-	;;
-
+	type=$(uname -m)
+	case "${type}" in
+		x86) yes | cp -rf ${RXTX_FOLDER}/x86/* ${RXTX_SHARED_OBJECT_LOCATION};;
+		x86_64) yes | cp -rf ${RXTX_FOLDER}/x86_64/* ${RXTX_SHARED_OBJECT_LOCATION};;
+		arm*) yes | cp -rf ${RXTX_FOLDER}/armhf/* ${RXTX_SHARED_OBJECT_LOCATION};;
+		*) echo "${type} isn't supported for RXTX Library installation!";;
+	esac
+    Mac) yes | cp -rf ${RXTX_FOLDER}/mac/* ${RXTX_SHARED_OBJECT_LOCATION};;
     *) echo "${machine} isn't supported for RXTX Library installation!";;
 esac
 
@@ -100,11 +76,11 @@ if [[ -f /etc/udev/rules.d/99-usb-serial.rules ]]; then
     while read -r newUsbRule; do
         alreadyImplementedRule=0
         while read -r alreadySavedUsbRule; do
-	    if [[ $alreadySavedUsbRule == $newUsbRule ]]; then
+	    if [[ $alreadySavedUsbRule == $newUsbRule ]]
 	        alreadyImplementedRule=1
 	    fi
 	done < "/etc/udev/rules.d/99-usb-serial.rules"
-        if [[ $alreadyImplementedRule == 0 ]]; then
+        if [[ $alreadyImplementedRule == 0 ]]
 	    echo $newUsbRule >> /etc/udev/rules.d/99-usb-serial.rules
         fi
     done < "./util/99-usb-serial.rules"
