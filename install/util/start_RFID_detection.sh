31a418f24c15c4989ebee2c7964aeea705d0d6ce
Snolext <lars.bauhofer@gmail.com>
2018-07-12 16:25:46 +0200

    Update start_RFID_detection.sh

--- install/util/start_RFID_detection.sh
+++ install/util/start_RFID_detection.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-java -Djava.library.path=/usr/lib/jni -jar rfid-detection-0.0.1-jar-with-dependencies.jar
+java -Djava.library.path=/usr/lib/jni -jar rfid-detection-0.0.1-jar-with-dependencies.jar -dev "/dev/NordicIdSampoS1"
